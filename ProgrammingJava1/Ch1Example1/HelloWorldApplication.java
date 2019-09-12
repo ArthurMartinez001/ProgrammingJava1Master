dc7fc2868da04d3c925bc8f296404f3620ba5726
ArthurMartinez <arthurmartinez001@gmail.com>
2019-09-12 12:30:18 -0700

    1st Upload

---
+++ ProgrammingJava1/Ch1Example1/HelloWorldApplication.java
@@ -0,0 +1,10 @@
+
+public class HelloWorldApplication {
+//this method displays Hello World! and ends. 
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.exit(0); //exits the program with the value of 0
+
+	}
+
+} //end of class
